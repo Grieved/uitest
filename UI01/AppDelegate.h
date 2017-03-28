e77a7730e33e124e8458ac7f1b61f8d81902219a
王宇 <wangyu@wangyudemacbook-pro.local>
2017-03-28 22:12:50 +0800

    Initial Commit

---
+++ UI01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UI01
+//
+//  Created by 王宇 on 28/03/2017.
+//  Copyright © 2017 王宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UI01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UI01
+//
+//  Created by 王宇 on 28/03/2017.
+//  Copyright © 2017 王宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
